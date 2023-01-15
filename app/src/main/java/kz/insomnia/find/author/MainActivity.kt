91ab31fd9c9c8af2a72d3a2579ffd7c2b36cdd91
Nursultan Chipataev <chipatayev@gmail.com>
2023-01-15 14:43:06 +0600

    fix build types

--- app/src/main/java/kz/insomnia/find/author/MainActivity.kt
+++ app/src/main/java/kz/insomnia/find/author/MainActivity.kt
@@ -3,13 +3,20 @@ package kz.insomnia.find.author
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.*
 import kz.insomnia.find.author.ui.theme.FindAuthorTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,13 +36,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    FindAuthorTheme {
-        Greeting("Android")
+    var visible by remember {
+        mutableStateOf(true)
+    }
+    Column (modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        AnimatedVisibility(visible = visible) {
+            Text(text = "Hello, $name!")
+        }
+        Button(onClick = { visible = !visible }) {
+            Text("Click Me")
+        }
     }
 }
